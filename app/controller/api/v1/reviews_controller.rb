88e652d1c99605f774b4f31891a7e901789ad50b
Matt Brooks <mattbrooks2010@gmail.com>
2020-08-27 20:25:02 -0500

    Last Commit for the night

--- app/controller/api/v1/reviews_controller.rb
+++ app/controller/api/v1/reviews_controller.rb
@@ -1,19 +1,27 @@
 class ReviewsController < ApplicationController
     
     def index
-
+        @reviews = Review.all
+        render json :@reviews
     end
 
     def show
-
+        render json: @reviews
     end
 
     def create
-
+        @review = Review.new(review_params)
+        if @review.save
+            render json: @review, status: created
+        else
+            render json: @review.errors, status: :unprocessable_entity
     end
     
     def update
-
+        if @review.update(review_params)
+            render json: @review
+        else
+            render json: @review.errors, status: :unprocessable_entity
     end
 
     def destroy
@@ -23,7 +31,7 @@ class ReviewsController < ApplicationController
     private
 
     def review_params
-        params.require(:review).permit(:body, :movie_id, :user_id)
+        params.require(:review).permit(:body, :movie_id, :user_id, :rating)
     end
 
 end
