d31fc04915af5bc7e8388323498443d10564cd30
Matt Brooks <mattbrooks2010@gmail.com>
2020-10-01 20:24:53 -0500

    working!!!

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -8,7 +8,7 @@ class Api::V1::UsersController < ApplicationController
 
     def create
         @user = UsersService.register(params[:email], params[:first_name], params[:last_name], params[:nickname], params[:password], params[:password_confirmation])
-        render json: { error: "There was a problem saving your user" }, status: unprocessable_entity and return unless @user 
+        render json: { error: "There was a problem saving your user" }, status: :unprocessable_entity and return unless @user 
         render json: @user.profile, status: :ok
     end
 
